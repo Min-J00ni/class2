fec96f0d3fac218b7ed5b29b4387ccfb223fa2e5
Min-J00ni <nx30115@gmail.com>
2024-12-06 08:51:44 +0900

    커밋!

--- todo/vite.config.js
+++ todo/vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: "/class2/todo/",
-})
+  build:{
+    outDir: '../dist/todo',
+    emptyOutDir: false,
+  },
+});
